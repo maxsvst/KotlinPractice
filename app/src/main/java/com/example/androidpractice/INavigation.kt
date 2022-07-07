274ed0a4c8ea658b6108a5c70c06505be340abbf
Feklin Max <fioklin2015@mail.ru>
2022-07-07 13:23:48 +0300

    2-ое задание - реализация собственного Navigator

---
+++ app/src/main/java/com/example/androidpractice/INavigation.kt
@@ -0,0 +1,7 @@
+package com.example.androidpractice
+
+import androidx.navigation.NavDirections
+
+interface INavigation {
+     fun navigateTo(direction: NavDirections)
+}
--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.androidpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    class NavigationImplementation(private val fragment: Fragment): INavigation{
+        override fun navigateTo(direction: NavDirections) {
+            try{
+                fragment.findNavController().navigate(direction)
+            }
+            catch (exception: Exception){
+                Log.d("Error", exception.toString())
+            }
+        }
     }
 }
