a060e2204cd85864cc80b880a1d8b834193bd4df
Feklin Max <fioklin2015@mail.ru>
2022-07-08 21:45:30 +0300

    3-ье задание - передача аргументов в провайдер конструкатора ViewModel

--- app/src/main/java/com/example/androidpractice/LoginFragment.kt
+++ app/src/main/java/com/example/androidpractice/LoginFragment.kt
@@ -1,16 +1,16 @@
 package com.example.androidpractice
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.view.*
 
 
 class LoginFragment : Fragment() {
@@ -19,19 +19,33 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
-        val loginInput: EditText = view.findViewById(R.id.loginInput)
-        val passwordInput: EditText = view.findViewById(R.id.passwordInput)
-        val viewModel = ViewModelProvider(this)[LoginFragmentViewModel::class.java]
-        view.loginButton.setOnClickListener() {
-            val passwordInput = passwordInput.text.toString()
-            val loginInput = loginInput.text.toString()
-            if (passwordInput == loginInput && passwordInput != "" && loginInput != "") {
-                findNavController().navigate(LoginFragmentDirections.openWelcomeFragment(loginInput))
-            } else {
-                Toast.makeText(context, "Логин и пароль не совпадают", Toast.LENGTH_SHORT).show()
-            }
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+//    private val viewModel: LoginFragmentViewModel = ViewModelProvider(this)[LoginFragmentViewModel::class.java]
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mainActivity: FragmentActivity = requireActivity()
+        val navigator = mainActivity as Navigator
+        val viewModelProvider by lazy {
+            ViewModelProvider(this, object :ViewModelProvider.Factory{
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return LoginFragmentViewModel(navigator) as T
+                }
+            })
+        }
+       val loginFragmentViewModel: LoginFragmentViewModel by lazy{
+            viewModelProvider[LoginFragmentViewModel::class.java]
+        }
+        val loginInput: EditText? = view.findViewById(R.id.loginInput)
+        val passwordInput: EditText? = view.findViewById(R.id.passwordInput)
+        val loginButton: Button? = view.findViewById(R.id.loginButton)
+        loginButton?.setOnClickListener {
+            val login: String = loginInput?.text.toString()
+            val password: String = passwordInput?.text.toString()
+            loginFragmentViewModel.isLoginAndPasswordMatch(password, login)
+        }
+    }
+
 }
--- app/src/main/java/com/example/androidpractice/LoginFragmentViewModel.kt
+++ app/src/main/java/com/example/androidpractice/LoginFragmentViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.androidpractice
 
+import android.util.Log
+import android.widget.EditText
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class LoginFragmentViewModel: ViewModel() {
-    override fun onCleared() {
-        super.onCleared()
+class LoginFragmentViewModel(private val navigate: Navigator): ViewModel() {
+
+    fun isLoginAndPasswordMatch(password: String, login: String){
+        if (password == login && password != "" && login != ""){
+            navigate.navigateTo(LoginFragmentDirections.openWelcomeFragment(login))
+        }
+        else{
+            Log.d("Error", "Логин и пароль не совпадают")
+        }
     }
+
 }
