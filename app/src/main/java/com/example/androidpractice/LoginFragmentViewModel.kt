3553878d1e34597b6a74faea3e3b7499601bcd45
Feklin Max <fioklin2015@mail.ru>
2022-07-08 11:26:17 +0300

    merge

--- app/src/main/java/com/example/androidpractice/LoginFragmentViewModel.kt
+++ app/src/main/java/com/example/androidpractice/LoginFragmentViewModel.kt
@@ -1,8 +1,21 @@
 package com.example.androidpractice
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class LoginFragmentViewModel: ViewModel() {
+class LoginFragmentViewModel @Inject constructor(
+//    private val navigator: Navigator
+) : ViewModel() {
+    fun isLoginAndPasswordMatch(loginInput: String, passwordInput: String) {
+        if (passwordInput == loginInput && passwordInput != "" && loginInput != "") {
+
+        } else {
+            Log.d("Error", "Логин и пароль не совпадают")
+        }
+    }
+
     override fun onCleared() {
         super.onCleared()
     }
